670cc8453c1eb566ccfde5f2adea2d63a33e26c4
LoveLightnDark <100453891+LoveLightnDark@users.noreply.github.com>
2022-08-03 21:07:09 -0400

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,26 @@
+// random value generated
+  var y = Math.floor(Math.floor()* 10 + 1);
+  var x = document.getElementById("guessField").ariaValueMax;
+// counting the number of guesses
+// made for correct Guess
+
+if(x == y)
+{
+    alert("CONGRATULATIONS!!!"+playername+" YOU GUESSSED IT RIGHT IN"
+    + guess+ "GUESS!!!!! ");
+    guess= 1;
+}  
+else if(x > y)
+{
+  guess++;
+  alert("OOPS SORRY!! TRY A SMALLER NUMBER!!!");
+}
+else
+{
+  guess++;
+  alert("OOPS SORRY!! TRY A GREATER NUMBER!!!")
+}
+function playAgain() {
+  y = Math.floor(Math.floor()* 10 + 1);
+}
+// function for number guessed by user     
